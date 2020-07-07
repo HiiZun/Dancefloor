2a5dd0ba231dd2c5da392b6cde9f0da118c5faa5
HiiZun <HiiZun@protonmail.com>
2020-07-07 16:47:15 +0200

    prefix on the playing status

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ module.exports = class {
     }
 
     async run() {
-        this.client.user.setActivity(`p?help | ${this.client.guilds.cache.size} Guilds | ${this.client.users.cache.size} Users`, {
+        this.client.user.setActivity(`d!help | ${this.client.guilds.cache.size} Guilds | ${this.client.users.cache.size} Users`, {
             type: "WATCHING"
         });
         this.client.logger.log(`${this.client.user.tag}, ready to serve ${this.client.users.cache.size} users in ${this.client.guilds.cache.size} servers.`, "ready");
