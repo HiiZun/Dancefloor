1df7fb3be026023402f2bfcba5702e770c5a5c62
HiiZun <HiiZun@protonmail.com>
2020-07-07 16:42:52 +0200

    just adding a webserver if running on heroku

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const { Player } = require("discord-player");
 
 require("dotenv").config()
 
+if(process.env.PORT){
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(process.env.PORT); //the server object listens on port 8080
+}
+
 class Dancefloor extends Client {
   /**
    * @constructor
